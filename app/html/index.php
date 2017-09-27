abdfa125e722cfb7cd8a9260e4c4e7d1fdb8b678
sukhstars <sukhy87@me.com>
2017-09-27 10:24:28 +0100

    atoms

--- app/html/index.php
+++ app/html/index.php
@@ -3,14 +3,13 @@
   <head>
     <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>title</title>
-  <meta name="author" content="name">
+    <title> Avantis Insurance </title>
+  <meta name="Sukhvir Singh" content="Sukhvir Singh">
   <meta name="description" content="description here">
   <meta name="keywords" content="keywords,here">
   <link rel="stylesheet" href="../css/main.css" type="text/css">
   </head>
   <body>
-    <h1 class="h1"> h1 tag </h1>
-    <button class="btn btn-custom"> button </button>
+   
   </body>
 </html>
