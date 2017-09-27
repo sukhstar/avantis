9838e99c0699b67d9484a7e8ac743053d4cda89f
sukhstars <sukhy87@me.com>
2017-09-27 20:15:29 +0100

    atoms and molecules

---
+++ app/js/custom-js.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 53.416515, lng: -2.169856};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
